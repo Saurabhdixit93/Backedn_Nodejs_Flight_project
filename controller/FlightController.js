f6c66a25d340cf2b97b02b86c400ff03312b7849
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-08-05 11:16:32 +0000

    Update FlightController.js

--- controller/FlightController.js
+++ controller/FlightController.js
@@ -1,6 +1,43 @@
 /* Importing All Files */
 
 const FlightSaveModel = require("../models/FlightSaveModel");
+const API_KEY = process.env.AVIATION_STACK_API_KEY;
+const API_URL = "http://api.aviationstack.com/v1";
+const axios = require("axios");
+
+/* API Endpoint to fetch flight prices*/
+
+module.exports.GetPrices = async (req, res) => {
+  const { source, destination, date } = req.body;
+  // Check if any required field is empty
+  if (!source || !destination || !date) {
+    return res.json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+  try {
+    const response = await axios.get(
+      `${API_URL}/flights?access_key=${API_KEY}&dep_iata=${source}&arr_iata=${destination}&flight_date=${date}`
+    );
+    // Process the response and extract the flight prices
+    const flightPrices = response.data.data.map((flight) => ({
+      airline: flight.airline.iata,
+      price: flight.flight_price,
+    }));
+    return res.json({
+      success: true,
+      message: "Flight Fetched",
+      flightPrices,
+    });
+  } catch (error) {
+    console.log("Error In Fetching Flights Details", error);
+    return res.json({
+      success: false,
+      message: "Inernal Server Error",
+    });
+  }
+};
 
 /* Flight details Saving In User Specific*/
 module.exports.SaveFlighDetails = async (req, res) => {
